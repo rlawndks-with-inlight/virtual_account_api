214bf5671983e50b7ca2f342e15e02d12e5475c0
rlawndks4 <rlawndks0423@naver.com>
2023-12-29 14:28:44 +0900

    update

--- utils.js/corp-util/bankners.js
+++ utils.js/corp-util/bankners.js
@@ -245,6 +245,7 @@ export const banknersApi = {
                 let { data: result } = await axios.get(`https://${API_URL}/api/balance/info?${query}`, {
                     headers: makeHeaderData(dns_data, pay_type, decode_user)
                 })
+                console.log(result)
                 if (result?.code != '0000') {
                     return {
                         code: -100,
