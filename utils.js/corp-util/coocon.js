abeec8831492ed5ac373d826f0bc3fbeca7840e8
rlawndks4 <rlawndks0423@naver.com>
2024-01-08 03:23:18 +0900

    update

--- utils.js/corp-util/coocon.js
+++ utils.js/corp-util/coocon.js
@@ -7,7 +7,7 @@ const API_URL = process.env.NODE_ENV == 'production' ? "https://dev2.coocon.co.k
 
 const getDefaultBody = (dns_data, pay_type) => {
     return {
-        "SECR_KEY": 'dns_data[`${pay_type}_sign_key`]',
+        "SECR_KEY": dns_data[`${pay_type}_sign_key`],
         "TRT_INST_CD": '08945816',
         "BANK_CD": '089',
         "TRSC_SEQ_NO": (new Date().getTime()).toString().substring(0, 12)
