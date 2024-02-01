c9d5fd57a85b3d1b7959f22e99362a21575b6571
rlawndks4 <rlawndks0423@naver.com>
2024-02-02 01:10:52 +0900

    update

--- utils.js/socket/index.js
+++ utils.js/socket/index.js
@@ -9,16 +9,22 @@ export const emitSocket = async (item) => {
     let { method, data = {}, brand_id } = item;
     try {
         let title = '';
+        let message = '';
+        let link = '';
         if (method == 'deposit') {
             title = '입금건이 들어왔습니다.';
+            message = `${data?.nickname ?? ""} ${data?.deposit_acct_name}님이 ${commarNumber(data?.amount)} 원을 입금하였습니다.`;
+            link = `/manager/deposit/list`
             let result = await insertQuery(`bell_contents`, {
                 brand_id,
                 user_id: data?.user_id ?? 0,
                 title,
-                message: `${data?.nickname ?? ""} ${data?.deposit_acct_name}님이 ${commarNumber(data?.amount)} 원을 입금하였습니다.`,
-                link: `/manager/deposit/list`,
+                message,
+                link,
             })
             data['id'] = result?.result?.insertId;
+            data['message'] = message;
+            data['link'] = link;
         }
         socket.emit("message", { method, data, brand_id, title });
     } catch (err) {
