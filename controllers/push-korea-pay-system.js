e942987907dcb40ea0f33c747c973fc7791346f1
rlawndks4 <rlawndks0423@naver.com>
2024-03-13 01:57:07 +0900

    update

--- controllers/push-korea-pay-system.js
+++ controllers/push-korea-pay-system.js
@@ -103,11 +103,20 @@ const pushKoreaPaySystemCtrl = {
                 if (!exist_deposit) {
                     let result = await insertQuery(`deposits`, obj);
                     deposit_id = result?.result?.insertId;
+                    sendTelegramBot(dns_data, `${dns_data?.name}\n${mcht?.nickname} ${virtual_account?.deposit_acct_name} 님이 ${commarNumber(amount)}원을 입금하였습니다.`, JSON.parse(mcht?.telegram_chat_ids ?? '[]'));
+                    let bell_data = {
+                        amount,
+                        user_id: mcht?.id,
+                        deposit_acct_name,
+                        nickname: mcht?.nickname,
+                    }
+                    emitSocket({
+                        method: 'deposit',
+                        brand_id: dns_data?.id,
+                        data: bell_data
+                    })
                 }
-            } else {
-
             }
-            sendTelegramBot(dns_data, `${dns_data?.name}\n${mcht?.nickname} ${virtual_account?.deposit_acct_name} 님이 ${commarNumber(amount)}원을 입금하였습니다.`, JSON.parse(mcht?.telegram_chat_ids ?? '[]'));
             insertResponseLog(req, '0000');
             return res.send('0000');
         } catch (err) {
