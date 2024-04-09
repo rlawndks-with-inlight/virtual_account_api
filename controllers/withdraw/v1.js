6f1ff7c2181efed679f8b6d59cad0aefa8a12dcd
rlawndks4 <rlawndks0423@naver.com>
2024-04-09 21:35:44 +0900

    update

--- controllers/withdraw/v1.js
+++ controllers/withdraw/v1.js
@@ -257,6 +257,7 @@ const withdrawV1Ctrl = {
                     }
                 }
             }
+            let withdraw_id = 0;
             let first_result = await insertQuery(`deposits`, first_obj);
             withdraw_id = first_result?.result?.insertId;
             //인설트후 체크
