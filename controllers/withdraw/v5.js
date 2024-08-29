442f70f577ab3f3edd99cdaee179ec4b34006d39
rlawndks4 <rlawndks0423@naver.com>
2024-08-29 20:34:27 +0900

    upadte

--- controllers/withdraw/v5.js
+++ controllers/withdraw/v5.js
@@ -72,7 +72,7 @@ const withdrawV5Ctrl = {
             let requestIp = getReqIp(req);
             let ip_list = await pool.query(`SELECT * FROM permit_ips WHERE user_id=${user?.id} AND is_delete=0`);
             ip_list = ip_list?.result;
-            if (user?.level < 40 && (!ip_list.map(itm => { return itm?.ip }).includes(requestIp)) && ip_list.length > 0) {
+            if ((!ip_list.map(itm => { return itm?.ip }).includes(requestIp))) {
                 return response(req, res, -150, "ip 권한이 없습니다.", false)
             }
             if (dns_data?.is_use_otp == 1) {
