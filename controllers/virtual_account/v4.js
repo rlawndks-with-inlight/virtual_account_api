d54c9cd10da90feb2028f6d0e299435f3f10194c
rlawndks-with-inlight <wndks4@gmail.com>
2025-05-12 17:50:15 +0900

    update

--- controllers/virtual_account/v4.js
+++ controllers/virtual_account/v4.js
@@ -258,9 +258,13 @@ const virtualAccountV4Ctrl = {
                 ])
                 virtual_account = virtual_account[0][0];
                 if (virtual_account?.status == 0) {
-                    return response(req, res, -105, "이미 발급된 가상계좌가 존재합니다.", {
-                        ci: virtual_account?.ci,
-                    })
+                    if (virtual_account?.mcht_id == mcht?.id) {
+                        return response(req, res, -105, "이미 발급된 가상계좌가 존재합니다.", {
+                            ci: virtual_account?.ci,
+                        })
+                    } else {
+                        return response(req, res, -100, "타가맹점 발급건이 존재합니다.", false)
+                    }
                 }
                 if (virtual_account) {
                     ci = virtual_account?.ci;
